d588e32b5e1aa7e464bf42a965bece04cbf7a701
Akash Vadnala <67157125+akashvadnala@users.noreply.github.com>
2022-02-18 09:39:42 +0530

    Create Nearest greater.cpp

---
+++ Use Stack instead BruteForce method/Nearest greater.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> nearestgreater(vector<int>& arr){
+    vector<int> ans(arr.size(),-1);
+    for(int i=0;i<arr.size();i++){
+        while(!st.empty() && arr[st.top()]<arr[i]){
+            ans[st.top()]=arr[i];
+            st.pop();
+        }
+        st.push(i);
+    }
+    return ans;
+}
+int main(){
+    vector<int> arr{1,0,2,1,4,5,0,2};
+    nearestgreater(arr);
+    return 0;
+}
