45c72b5359421aebb04ea4db9198ab48dbe6fa05
Akash Vadnala <67157125+akashvadnala@users.noreply.github.com>
2022-02-19 13:09:08 +0530

    Update Subset Sum.cpp

--- Dynamic Programming/1. 0-1 Knapsack Problem/Subset Sum.cpp
+++ Dynamic Programming/1. 0-1 Knapsack Problem/Subset Sum.cpp
@@ -1,13 +1,31 @@
 class Solution{
-    bool knapsack(vector<int>& nums,int i,int target){
-        if(target==0) return true;
-        if(i>=nums.size()) return false;
-        if(knapsack(nums,i+1,target-nums[i])) return true;
-        if(knapsack(nums,i+1,target)) return true;
-        return false;
-    }
+    // bool knapsack(vector<int>& nums,int i,int target){
+    //     if(target==0) return true;
+    //     if(i>=nums.size()) return false;
+    //     if(knapsack(nums,i+1,target-nums[i])) return true;
+    //     if(knapsack(nums,i+1,target)) return true;
+    //     return false;
+    // }
 public:
     bool isSubset(vector<int>& nums,int& target){
-        return knapsack(nums,0,target);
+        //return knapsack(nums,0,target);
+        int n=nums.size();
+        vector<vector<bool>> dp(n+1,vector<bool>(target+1));
+        for(int i = 0; i < dp.size();i++) {
+            for(int j = 0;j < dp[0].size();j++) {
+                if(i == 0 && j == 0) dp[i][j] = true;
+                else if(i == 0) dp[i][j] = false;
+                else if(j == 0) dp[i][j] = true;
+                else {
+                    dp[i][j] = dp[i-1][j];
+                    if(j >= nums[i-1]) {
+                        if(dp[i-1][j-nums[i-1]] == true) {
+                            dp[i][j] = true;
+                        }
+                    }
+                }
+            }
+        }
+        return dp[n][target];
     }
 };
