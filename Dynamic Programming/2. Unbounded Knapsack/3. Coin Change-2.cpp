80d9fb3e14ca868744b40dbceb1e35552bfadc74
Akash Vadnala <67157125+akashvadnala@users.noreply.github.com>
2022-02-22 23:10:23 +0530

    Create 3. Coin Change-2.cpp

---
+++ Dynamic Programming/2. Unbounded Knapsack/3. Coin Change-2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int dp[amount+1];
+        for(int i=0;i<=amount;i++) dp[i]=0;
+        dp[0]=1;
+        for(int coin:coins){
+            for(int i=coin;i<=amount;i++)
+                dp[i]+=dp[i-coin];
+        }
+        return dp[amount];
+    }
+};
